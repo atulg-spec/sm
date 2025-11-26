144503f07f919b2bcd534bcbbd6f081baeded516
atul <atulg0736@gmail.com>
2025-11-26 23:53:23 +0530

    fix

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -2,9 +2,10 @@
 URL configuration for ecommerce project.
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 from django.contrib.sitemaps.views import sitemap
 from django.views.generic.base import TemplateView
@@ -24,7 +25,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 else:
     urlpatterns += [
