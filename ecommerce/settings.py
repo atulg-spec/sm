739cd22be542bb74a9a1c44e119b5991ca57ce4c
atul <atulg0736@gmail.com>
2025-11-28 22:04:18 +0530

    admin panel

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,6 +29,7 @@ CSRF_TRUSTED_ORIGINS = ['https://startmarket.in', 'https://www.startmarket.in']
 # Application definition
 
 INSTALLED_APPS = [
+    'paper_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
